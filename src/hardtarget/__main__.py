631b84b9063bb560f9692822a38cdac5fffa2bc9
Vetle Hofsoy-Woie <vho023@uit.no>
2021-06-16 13:36:25 +0200

    make it possible to run gpu code by supplying cmd line arguments

--- src/hardtarget/__main__.py
+++ src/hardtarget/__main__.py
@@ -1,2 +1,19 @@
 from . import run_sim
-run_sim.start_sim
+from . import __path__
+import sys
+
+print(__path__[0])
+
+gpu = False
+try:
+    if sys.argv[1] == "gpu":
+        gpu=True
+        print("Running simulator using GPU optimized code")
+    else:
+        print("Running simulator using serial code")
+except IndexError:
+    gpu = False
+    print("Running simulator using serial code")
+
+
+run_sim.start_sim(gpu)
