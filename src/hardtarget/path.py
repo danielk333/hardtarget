63ec572869259939179199f988306ff5bd98fdea
Vetle Hofsoy-Woie <vho023@uit.no>
2021-06-16 13:36:34 +0200

    remove path file

--- src/hardtarget/path.py
+++
@@ -1,8 +0,0 @@
-'''
-Exposes a global PATH variable to find the path to where the 
-module is installed.
-'''
-import os
-#Quite hacky way of finding the modules path. Tested on both 
-#Windows and Ubuntu so should work for most file systems
-_PATH = os.path.abspath(__file__)[:-len(__file__.split('/')[-1])]
