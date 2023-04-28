90e41ece47464d361914ea74e6f13b25e33273c5
Ingar Arntzen <ingar.arntzen@gmail.com>
2023-04-28 15:51:53 +0200

    fixed cuda test

--- src/gmf_cuda_lib/testcuda.py
+++ src/gmf_cuda_lib/testcuda.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import subprocess
 
-print("Testing Cuda. This may take a few seconds... ")
+print("Testing Cuda... ")
 subprocess.run(["mkdir", "-p", "/tmp"])
 subprocess.run(["nvcc", "hello.cu", "-o", "/tmp/hello"])
 result = subprocess.run(["/tmp/hello"], stdout=subprocess.PIPE, text=True)
 subprocess.run(["rm", "/tmp/hello"])
 
-if result.stdout == "Hello Hello \n":
+if result.stdout == "Hello World \n":
     print("Cuda is running!")
 else:
     print("Cuda is not running!")
