8577cd191bcc313d2163569caaea89bfd3d40552
Ingar Arntzen <ingar.arntzen@gmail.com>
2023-05-03 13:51:52 +0200

    added entrypoint cudacheck

---
+++ src/hardtarget/tools/cudacheck.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+
+def main():
+
+    dirpath = os.path.dirname(os.path.realpath(__file__))
+    srcfile = os.path.join(dirpath, "cudacheck.cu")
+
+    print("Checking Cuda... ")
+    subprocess.run(["mkdir", "-p", "/tmp"])
+    subprocess.run(["nvcc", srcfile, "-o", "/tmp/hello"])
+    result = subprocess.run(["/tmp/hello"], stdout=subprocess.PIPE, text=True)
+    subprocess.run(["rm", "/tmp/hello"])
+
+    if result.stdout == "Hello World \n":
+        print("Cuda is running!")
+    else:
+        print("Cuda is NOT running!")
+
+
+if __name__ == '__main__':
+    main()
