00aada4d7528a4580e66dcda84094462d45d28e7
danielk <daniel.kastinen@irf.se>
2023-06-12 11:02:23 +0200

    black formatting

--- src/hardtarget/utilities.py
+++ src/hardtarget/utilities.py
@@ -1,8 +1,11 @@
 # Stuff that is useful in several places in hardtarget library
 
+
 # Juha's convenience function for reading 1d vector of c8 values from a digital_rf source
 def read_vector_c81d(rdf_reader, *args, **kws):
-    return rdf_reader.read_vector_1d(*args, **kws).astype('c8', casting='unsafe', copy=False)
+    return rdf_reader.read_vector_1d(*args, **kws).astype(
+        "c8", casting="unsafe", copy=False
+    )
 
 
 # From Stuffr
