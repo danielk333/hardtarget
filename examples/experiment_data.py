2ed4888562c43825e9ff5d095bfbccbcebbe3312
danielk <daniel.kastinen@irf.se>
2023-06-16 12:35:15 +0200

    experiment package data

---
+++ examples/experiment_data.py
@@ -0,0 +1,9 @@
+"""
+Predefined experiment data
+==========================
+"""
+
+import hardtarget.experiments as ex
+
+print("Package data files avalible:")
+print(ex.DATA)
---
+++ src/hardtarget/experiments/__init__.py
@@ -0,0 +1,27 @@
+import pathlib
+import importlib.resources
+
+DATA = {}
+
+# To be compatible with 3.7-8
+# as resources.files was introduced in 3.9
+if hasattr(importlib.resources, "files"):
+    _data_folder = importlib.resources.files("hardtarget.experiments")
+    for file in _data_folder.iterdir():
+        if not file.is_file():
+            continue
+        if file.name.endswith(".py"):
+            continue
+
+        DATA[file.name] = file
+
+else:
+    _data_folder = importlib.resources.contents("hardtarget.experiments")
+    for fname in _data_folder:
+        with importlib.resources.path("hardtarget.experiments", fname) as file:
+            if not file.is_file():
+                continue
+            if file.name.endswith(".py"):
+                continue
+
+            DATA[file.name] = pathlib.Path(str(file))
