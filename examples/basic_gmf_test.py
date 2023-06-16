baac72a6f055ea862233c38ff028d6113c3d3392
Ingar Arntzen <ingar.arntzen@gmail.com>
2023-06-16 08:56:52 +0200

    fixed up examples/basic

--- examples/basic_gmf_test.py
+++ examples/basic_gmf_test.py
@@ -2,31 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-from hardtarget.analysis.gmf import GMF_LIBS
+from hardtarget.gmf import GMF_LIBS
 
 gmf = GMF_LIBS['c']
 
-z_tx=np.zeros(10000,dtype=np.complex64)
-z_rx=np.zeros(12000,dtype=np.complex64)    
+z_tx = np.zeros(10000, dtype=np.complex64)
+z_rx = np.zeros(12000, dtype=np.complex64)
 for i in range(10):
     z_tx[(i*1000):(i*1000+20)]=1.0
-    z_rx[(i*1000+500):(i*1000+(500+20))]=0.5 # simulated "echo"
+    z_rx[(i*1000+500):(i*1000+(500+20))] = 0.5  # simulated "echo"
 
-dec=10
-acc_phasors=np.zeros([20,1000],dtype=np.complex64)
-acc_phasors[0,:]=1.0
-rgs=np.zeros(1000,dtype=np.float32)#arange(700,dtype=np.int64)
+dec = 10
+acc_phasors = np.zeros([20, 1000], dtype=np.complex64)
+acc_phasors[0, :] = 1.0
+rgs = np.zeros(1000, dtype=np.float32)  # arange(700,dtype=np.int64)
 for ri in range(len(rgs)):
-    rgs[ri]=ri
-n_r=len(rgs)
-gmf_vec=np.zeros(n_r,dtype=np.float32);
-gmf_dc_vec=np.zeros(n_r,dtype=np.float32);
-v_vec=np.zeros(n_r,dtype=np.float32);
-a_vec=np.zeros(n_r,dtype=np.float32);
-cput0=time.time()
+    rgs[ri] = ri
+n_r = len(rgs)
+gmf_vec = np.zeros(n_r, dtype=np.float32)
+gmf_dc_vec = np.zeros(n_r, dtype=np.float32)
+v_vec = np.zeros(n_r, dtype=np.float32)
+a_vec = np.zeros(n_r, dtype=np.float32)
+cput0 = time.time()
 for i in range(20):
-    gmf(z_tx,z_rx,acc_phasors,rgs,dec,gmf_vec,gmf_dc_vec,v_vec,a_vec)
-cput1=time.time()
+    gmf(z_tx, z_rx, acc_phasors, rgs, dec, gmf_vec, gmf_dc_vec, v_vec, a_vec)
+cput1 = time.time()
 plt.plot(gmf_vec)
 plt.show()
 plt.plot(gmf_dc_vec)
@@ -35,6 +35,6 @@ plt.plot(v_vec)
 plt.show()
 plt.plot(a_vec)
 plt.show()
-print("Execution time %1.2f"%(cput1-cput0))
-ri=np.argmax(gmf_vec)
-print("Rmax %d gmf %1.2g v %1.2f a %1.2f"%(ri,gmf_vec[ri],v_vec[ri],a_vec[ri]))
+print(f"Execution time {cput1-cput0:1.2f}")
+ri = np.argmax(gmf_vec)
+print(f"Rmax {ri} gmf {gmf_vec[ri]:1.2g} v {v_vec[ri]:1.2f} a {a_vec[ri]:1.2f}")
