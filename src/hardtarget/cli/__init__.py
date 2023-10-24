dd4467405c887af26a6fa270209e1b29945afe44
danielk <daniel.kastinen@irf.se>
2023-10-24 16:07:59 +0200

    added drfinfo command to central cli

--- src/hardtarget/cli/__init__.py
+++ src/hardtarget/cli/__init__.py
@@ -1,6 +1,7 @@
 #This is needed so that the registration is performed
 from . import convert
 from . import analyze
+from . import drfinfo
 
 #Then expose the main after registration
 from . commands import main
