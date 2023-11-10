f8f0ba923f4a0ed6240b0cb869c0055eadaedfba
danielk <daniel.kastinen@irf.se>
2023-11-10 09:05:27 +0100

    updated programatic rti plot example

--- examples/drf_RTI_plot.py
+++ examples/drf_RTI_plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import hardtarget
 
-target = "/home/danielk/data/spade/beamparks_raw/leo_bpark_2.1u_NO@uhf/"
+target = "/home/danielk/data/spade/beamparks_raw/leo_bpark_2.1u_NO@uhf_drf/"
 reader, params = hardtarget.load_hardtarget_drf(target)
 
 
@@ -10,11 +10,11 @@ ax, handles = hardtarget.plotting.rti(
     ax,
     reader,
     params,
-    "uhf",
-    start_time=2.0,
-    end_time=8.0,
+    start_time=2.0,  # s
+    end_time=8.0,  # s
     relative_time=True,
     axis_units=True,
+    clutter_removal=2500.0e-6,  # s
 )
 
 plt.show()
