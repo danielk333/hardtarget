dba76480e1a41da9da9909b42541730b413927c0
danielk <daniel.kastinen@irf.se>
2023-11-15 07:30:23 +0100

    started on gmf plotting functions

--- src/hardtarget/plotting/gmf.py
+++ src/hardtarget/plotting/gmf.py
@@ -1,59 +1,58 @@
-import glob
+import pathlib
 import sys
 import h5py
 import matplotlib.pyplot as plt
-import numpy as n
-import scipy.constants as c
+import numpy as np
+import scipy.constants as consts
 
 
-def get_noise_floor(conf):
-    fl = glob.glob("%s/*/gmf*.h5" % (conf.output_dir))
+def get_noise_floor(path):
+    fl = list(path.glob("*/gmf*.h5"))
     fl.sort()
+    print(fl)
     floors = []
     for f in fl:
         print(f)
         h = h5py.File(f, "r")
         gmf = h["gmf"][()]
-        floors.append(n.nanmedian(gmf, axis=0))
+        floors.append(np.nanmedian(gmf, axis=0))
         h.close()
-    floors = n.array(floors)
-    return n.nanmedian(floors, axis=0)
+    floors = np.array(floors)
+    return np.nanmedian(floors, axis=0)
 
 
-def plot_gmfs(conf, nf):
-    fl = glob.glob("%s/*/gmf*.h5" % (conf.output_dir))
+def plot_gmfs(path, nf):
+    fl = list(path.glob("*/gmf*.h5"))
     fl.sort()
     print(len(fl))
     for f in fl:
         print(f)
         h = h5py.File(f, "r")
-        gmf = n.copy(h["gmf"][()])
-        v = n.copy(h["v"][()])
-        a = n.copy(h["a"][()])
-
+        gmf = h["gmf"][()]
+        v = h["v"][()]
+        a = h["a"][()]
+        breakpoint()
         for i in range(gmf.shape[0]):
             gmf[i, :] = (gmf[i, :] - nf) / nf
-        #            plt.plot(gmf[i,:])
-        #           plt.show()
 
-        if n.max(gmf) > 10.0:
+        if np.max(gmf) > 10.0:
             plt.subplot(221)
-            plt.plot(n.max(gmf, axis=1))
+            plt.plot(np.max(gmf, axis=1))
             plt.subplot(222)
-            dr = c.c / conf.sample_rate / 2.0 / 1e3
-            plt.plot(dr * conf.rgs[n.argmax(gmf, axis=1)], ".")
+            dr = consts.c / h.attrs["sample_rate"] / 2.0 / 1e3
+            plt.plot(dr * h["vector_params"]["rgs"][np.argmax(gmf, axis=1)], ".")
             plt.subplot(223)
 
             vels = []
             for i in range(gmf.shape[0]):
-                mi = n.argmax(gmf[i, :])
+                mi = np.argmax(gmf[i, :])
                 vels.append(v[i, mi])
 
             plt.plot(vels, ".")
             plt.subplot(224)
             ass = []
             for i in range(gmf.shape[0]):
-                mi = n.argmax(gmf[i, :])
+                mi = np.argmax(gmf[i, :])
                 ass.append(a[i, mi])
 
             plt.plot(ass, ".")
@@ -64,12 +63,12 @@ def plot_gmfs(conf, nf):
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         print(sys.argv[1])
-        conf = None
+        path = pathlib.Path(sys.argv[1]).resolve()
     else:
-        print("Provide configuration file as command line option")
+        print("Provide path as command line option")
         exit(0)
-    nf = get_noise_floor(conf)
+    nf = get_noise_floor(path)
     nf[nf < 1] = 1.0
     plt.plot(nf)
     plt.show()
-    plot_gmfs(conf, nf)
+    plot_gmfs(path, nf)
