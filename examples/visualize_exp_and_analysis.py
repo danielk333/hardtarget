276aa274f0f8dd014e5a8a57db8b130a1aef2627
danielk <daniel.kastinen@irf.se>
2023-11-15 07:43:16 +0100

    visulize experiment

--- examples/visualize_exp_and_analysis.py
+++ examples/visualize_exp_and_analysis.py
@@ -11,7 +11,7 @@ t = np.arange(params["read_length"], dtype=np.float64)/params["sample_rate"]
 
 vline_st = dict(ls="--", alpha=0.5)
 
-fig, axes = plt.subplots(2, 3)
+fig, axes = plt.subplots(2, 2)
 
 axes[0, 0].plot(t, params["rx_stencil"], "-b", label="RX")
 axes[0, 0].plot(t, params["tx_stencil"], "-r", label="TX")
@@ -27,5 +27,24 @@ for ind in range(params["n_ipp"]):
 
 axes[0, 0].set_title("Stencils")
 axes[0, 0].legend()
+
+axes[1, 0].plot(t, params["rx_stencil"], "-b", label="RX")
+axes[1, 0].plot(t, params["tx_stencil"], "-r", label="TX")
+axes[1, 0].axvline(params["tx_start"]*1e-6, c="r", **vline_st)
+axes[1, 0].axvline(params["tx_end"]*1e-6, c="r", **vline_st)
+axes[1, 0].axvline(params["rx_start"]*1e-6, c="b", **vline_st)
+axes[1, 0].axvline(params["rx_end"]*1e-6, c="b", **vline_st)
+axes[1, 0].axvline(params["cal_on"]*1e-6, c="g", **vline_st)
+axes[1, 0].axvline(params["cal_off"]*1e-6, c="g", **vline_st)
+axes[1, 0].set_xlim(0, params["ipp"]*1e-6)
+axes[0, 0].set_title("Stencils - single")
+axes[0, 0].legend()
+
+
+axes[0, 1].pcolormesh(np.real(params["acceleration_phasors"]))
+axes[0, 1].set_title("Acceleration phasors - real")
+
+axes[1, 1].pcolormesh(np.imag(params["acceleration_phasors"]))
+axes[1, 1].set_title("Acceleration phasors - imag")
 
 plt.show()
