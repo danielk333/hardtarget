a55146ce23996afb7fbeb5f72a19b1a5099b2c25
danielk <daniel.kastinen@irf.se>
2023-11-23 17:15:39 +0100

    docs for testing gmf programmatically and debugging

--- examples/run_gmf_programmatically.py
+++ examples/run_gmf_programmatically.py
@@ -1,9 +1,44 @@
+#!/usr/bin/env python
+"""
+Run analysis programmatically
+==============================
+
+This script shows how to call the `compute_gmf` function directly.
+
+For developers this is also particularly useful since it can be used to
+debug the backends developed in different programming languages.
+
+For example, to debug the C-implementation using GNU debugger simply
+
+```
+gdb --args python ./examples/run_gmf_programmatically.py
+```
+
+to start gdb targeting the python binary which is set to run this example.
+Then we need to load the debug symbols from the correct binary to set
+breakpoints, we can do this (if this is installed in developer-mode using the
+pip `-e` option) by typing these commands into gdb
+
+```
+add-symbol-file ./src/hardtarget/gmf/gmfclib.cpython-311-x86_64-linux-gnu.so
+break gmf.c:gmf
+run
+```
+
+which will run this file and halt execution at the `gmf` function in the C
+implementation in `gmf.c` and allow you to debug the compiled c-code as it is
+exactly loaded and run by python.
+
+"""
+
+
 from pathlib import Path
 from pprint import pprint
 import hardtarget
 
 try:
     from mpi4py import MPI
+
     rank = MPI.COMM_WORLD.rank
     size = MPI.COMM_WORLD.size
 except ImportError:
