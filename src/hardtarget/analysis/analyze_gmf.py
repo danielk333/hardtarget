0b77ee642acc8c301075a443a9891dda41440de5
danielk <daniel.kastinen@irf.se>
2023-11-23 08:56:51 +0100

    combined files and renamed function for better scoping and consistency

--- src/hardtarget/analysis/analyze_gmf.py
+++ src/hardtarget/analysis/analyze_gmf.py
@@ -6,9 +6,10 @@ import digital_rf as drf
 import h5py
 from tqdm import tqdm
 from pathlib import Path
-from hardtarget.analysis import analyze_ipps
+from hardtarget.gmf import GMF_LIBS
 from hardtarget import gmf_utils, drf_utils
 
+logger = logging.getLogger(__name__)
 
 ####################################################################
 # DIGITAL RF READERS
@@ -40,7 +41,7 @@ def load_source(src):
     # read cached instance
     reader = _DRF_READERS.get(path, None)
     if reader is None:
-        _DRF_READERS[path] = reader = drf.DigitalRFReader([path])
+        _DRF_READERS[path] = reader = drf.DigitalRFReader([str(path)])
     channels = reader.get_channels()
     if chnl not in channels:
         raise ValueError(f"reader does not support channel, {chnl}")
@@ -150,7 +151,7 @@ def _create_annotated_var(h5file, name, data, long_name, units=None):
         var.attrs["units"] = units
 
 
-def analyze_gmf(
+def compute_gmf(
     rx,
     tx,
     config=None,
@@ -158,7 +159,6 @@ def analyze_gmf(
     end_time=None,
     relative_time=False,
     job={"idx": 1, "N": 1},
-    logger=None,
     progress=False,
     progress_position=0,
     subprogress=True,
@@ -217,9 +217,6 @@ def analyze_gmf(
     job_tasks = compute_job_tasks(job, total_tasks)
     tasks_skipped = 0
 
-    # logging
-    if logger is None:
-        logger = logging.getLogger(__name__)
     logger.info(f"starting job {job['idx']}/{job['N']} with {len(job_tasks)} tasks")
 
     ipp = gmf_params["ipp"]
@@ -274,7 +271,7 @@ def analyze_gmf(
         ts0 = time.time()
         for i in range(num_cohints_per_file):
             start_sample = file_idx_sample + i * ipp_samp * n_ipp
-            gmf_vars, gmf_tx = analyze_ipps.analyze_ipps(
+            gmf_vars, gmf_tx = integrate_and_match_ipps(
                 (rx_reader, rx_chnl), (tx_reader, tx_chnl), start_sample, gmf_params
             )
             gmf_vals.append(gmf_vars.vals)
@@ -430,3 +427,94 @@ def analyze_gmf(
 
     logger.info(f"finishing job {job['idx']}/{job['N']} with {len(job_tasks)} tasks")
     return results
+
+
+####################################################################
+# ANALYSE IPPS
+####################################################################
+
+
+def integrate_and_match_ipps(rx, tx, start_sample, gmf_params):
+    """
+    TODO: clean up this and all other docstrings when structure is done
+
+    Analyse ipps runs the gmf function.
+
+    Parameters
+    ----------
+    rx : tuple
+        (DigitalRF.reader, str) the drf reader and name of the channel with the rx signal
+    tx : tuple
+        (DigitalRF.reader, str) the drf reader and name of the channel with the tx signal
+    start_sample : int
+        Start sample of integration period
+    gmf_params : dict
+        GMF parameters
+
+    Returns
+    -------
+    gmf_vars : hardtarget.gmf_utils.GMFVariables
+        Named tuple container for compacting the variables set by the GMF function.
+    tx_amp2 : float
+        Squared summed tx amplitude
+    """
+
+    # gmf lib
+    gmf_lib = gmf_params.get("gmflib", None)
+    if gmf_lib is None or gmf_lib not in GMF_LIBS:
+        gmf_lib = "c" if "c" in GMF_LIBS else "numpy"
+    gmf = GMF_LIBS[gmf_lib]
+
+    # TODO: implement this
+    if not gmf_params["reduce_range_rate"] and gmf_params["reduce_acceleration"]:
+        raise NotImplementedError("reduce settings not implemented")
+
+    # parameters
+    rx_stencil = gmf_params["rx_stencil"]
+    tx_stencil = gmf_params["tx_stencil"]
+
+    rx_reader, rx_channel = rx
+    tx_reader, tx_channel = tx
+
+    # read data vector with n_ipp + n_extra ipp's (to allow for searching across to subsequent pulses)
+    z_rx = rx_reader.read_vector_1d(start_sample, gmf_params["read_length"], rx_channel)
+
+    if tx_channel != rx_channel or tx_reader != rx_reader:
+        z_tx = tx_reader.read_vector_1d(start_sample, gmf_params["read_length"], tx_channel)
+    else:
+        z_tx = np.copy(z_rx)
+
+    # clean ground clutter, get separate transmit waveform and echo vectors
+    z_rx = z_rx[rx_stencil]
+    z_tx = z_tx[tx_stencil]
+
+    # conjugate, so that when matched filtering, it will cancel out phase of transmit waveform.
+    # scale transmit waveform to unity power
+    tx_amp2 = np.sum(np.abs(z_tx) ** 2.0)
+    tx_amp = np.sqrt(tx_amp2)
+    z_tx = np.conj(z_tx) / tx_amp
+
+    # TODO: Create a truncated transmission envelope based on if the tx power
+    # in the sample is > limit=1e-10 or not, then truncate the rx select window
+    # and acceleration phasors accordingly
+
+    # TODO: There are better ways of estimating the background noise by
+    #   removing all coherent echoes first and using the individual signal samples
+
+    gmf_vars = gmf_utils.GMFVariables(
+        vals = np.zeros(gmf_params["gmf_size"], dtype=np.float32),
+        dc = np.zeros([gmf_params["n_ranges"], ], dtype=np.float32),
+        r_ind = np.full(gmf_params["gmf_size"], -1, dtype=np.int32),
+        v_ind = np.full(gmf_params["gmf_size"], -1, dtype=np.int32),
+        a_ind = np.full(gmf_params["gmf_size"], -1, dtype=np.int32),
+    )
+
+    if tx_amp > 1.0:
+        gmf(
+            z_tx,
+            z_rx,
+            gmf_vars,
+            gmf_params,
+        )
+
+    return gmf_vars, tx_amp2
