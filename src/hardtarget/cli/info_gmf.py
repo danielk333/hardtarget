ac850d9f6456e424668d542111cdb63857a0eac3
Ingar Arntzen <ingar.arntzen@gmail.com>
2023-11-27 14:51:44 +0100

    some refactoring in info_gmf

--- src/hardtarget/cli/info_gmf.py
+++ src/hardtarget/cli/info_gmf.py
@@ -4,14 +4,20 @@ from pathlib import Path
 from .commands import add_command
 
 
-def parser_build(parser):
-    parser.add_argument("path", help="Path to source directory with GMF data")
-    return parser
+################################################################
+# PARSE GMF OUT (H5)
+#
+# Parse groups and datasets recursively
+################################################################
 
 
-def all_files(top):
+def is_scale(obj):
+    return obj.attrs.get("CLASS") == b"DIMENSION_SCALE"
+
+
+def all_h5_files(gmf_folder):
     """generate all files matching 'yyyy-mm-ddThh-00-00/gmf-*.h5'"""
-    top = Path(top)
+    top = Path(gmf_folder)
     dir_pattern = re.compile(r'\d{4}-\d{2}-\d{2}T\d{2}-00-00')
     subdirs = [d for d in top.iterdir() if d.is_dir() and dir_pattern.match(d.name)]
     file_pattern = re.compile(r'^gmf-.*\.h5$')
@@ -21,51 +27,65 @@ def all_files(top):
     return files
 
 
-def is_scale(obj):
-    return obj.attrs.get("CLASS") == b"DIMENSION_SCALE"
-
-
-def path_to_str(path):
-    return "".join([f"[{k}]" for k in path])
-
-
-def inspect_dataset(path, obj):
-    _path = path_to_str(path)
-    if (is_scale(obj)):
-        print(f"-- {_path} (scale) shape {obj.shape}, {obj.dtype}")
-    else:
-        print(f"-- {_path} (dataset) shape {obj.shape}, {obj.dtype}")
-
-
-def inspect_other(path, obj):
-    _path = ".".join(path)
-    print(f"-- [{_path}] shape {obj.shape}, {obj.dtype}")
-
-
-def inspect_node(path, obj):
-    if path is None:
-        path = []
-    for key, item in obj.items():
-        new_path = path + [key]
-        if isinstance(item, h5py.Group):
-            inspect_node(new_path, item)
-        elif isinstance(item, h5py.Dataset):
-            inspect_dataset(new_path, item)
+def inspect_h5_node(obj, path=[]):
+    items = []
+    for child_key, child_item in obj.items():
+        child_path = path + [child_key]
+        if isinstance(child_item, h5py.Group):
+            items += inspect_h5_node(child_item, child_path)
         else:
-            inspect_other(new_path, item)
+            items.append(inspect_h5_leaf(child_item, child_path))
+    return items
+
+
+def inspect_h5_leaf(obj, path):
+    item = {
+        "obj": obj
+    }
+    if isinstance(obj, h5py.Dataset):
+        item["type"] = "scale" if is_scale(obj) else "dataset"
+    else:
+        item["type"] = "other"
+    return path, item
+
+
+################################################################
+# MAIN
+################################################################
 
 
 def main(args):
 
-    files = all_files(args.path)
-    # choose one file
+    # find all h5 files in
+    files = all_h5_files(args.path)
+    # choose one f5 file from withih
     if len(files) == 0:
         print("Found no files at path", args.path)
         return
 
     file = files[0]
     with h5py.File(file, "r") as f:
-        inspect_node(None, f)
+        items = inspect_h5_node(f)
+
+        # print
+
+        def path_to_str(path):
+            return "".join([f"[{k}]" for k in path])
+
+        for path, item in items:
+            _path = path_to_str(path)
+            if item['type'] in ["dataset", "scale"]:
+                print(f"-- {_path} type:{item['type']} shape:{item['obj'].shape} dtype:{item['obj'].dtype}")
+
+
+################################################################
+# CLI INTEGRATION
+################################################################
+
+
+def parser_build(parser):
+    parser.add_argument("path", help="Path to source directory with GMF data")
+    return parser
 
 
 add_command(
