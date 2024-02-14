5394f77935c08cd23ae8f365db6042cc17cee3e6
danielk <daniel.kastinen@irf.se>
2024-02-14 19:31:07 +0100

    added some more plots to the tutorial to show relations

--- examples/back_to_basics/part5_ambiguity_function.py
+++ examples/back_to_basics/part5_ambiguity_function.py
@@ -11,86 +11,133 @@ accelerating signal.
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fft as fft
-import scipy.constants as constants
-
-
-sample_rate = 1000000
-signal_samps = 200000
-wavelength = 0.3
-frequency = constants.c / wavelength
-
-vel0 = 0.4e3
-dop0 = vel0 / wavelength
-omega0 = dop0 * 2 * np.pi
-acel0 = -0.20e3
-ddop0 = (acel0 * 0.5) / wavelength
-domega0 = ddop0 * 2 * np.pi
-
-s = np.arange(signal_samps)
-t = s / sample_rate
-
-r = vel0*t + acel0*0.5*t**2
-phase = np.mod(r / wavelength, 1) * np.pi * 2
-signal = np.exp(1j * phase)
 
 box_size = 6000
 cycle_size = 20000
-boxes = signal_samps // cycle_size
-tau0 = cycle_size * (boxes // 2)
+sample_rate = 1000000
 
-stensil = np.full((signal_samps, ), True, dtype=bool)
-for ind in range(boxes):
-    stensil[(cycle_size*ind):(cycle_size*ind + box_size)] = False
+wavelength = 0.3
+vel0 = 0.4e3
+dop0 = vel0 / wavelength
 
-signal[stensil] = 0
 
-fvec = fft.fftfreq(signal_samps, d=1.0/sample_rate)
-spec = np.abs(fft.fft(signal))
+def analyse_cycle_daf(cycles, acel0, plot=False):
+    ddop0 = (acel0 * 0.5) / wavelength
+    signal_samps = cycles*cycle_size
 
-dsignal = signal[tau0:] * np.conj(signal[:-tau0])
-# TODO: i dont remember why its 2*offset, can be seen in DAF-definition
-step = 2*tau0/sample_rate
-dsignal_samps = signal_samps - tau0
+    s = np.arange(signal_samps)
+    t = s / sample_rate
 
-# Divide this with the step size to get to Hz/s
-dfvec = fft.fftfreq(dsignal_samps, d=1.0/sample_rate) / step
-dspec = np.abs(fft.fft(dsignal))
-dspec_peak = np.argmax(dspec)
-dfreq_est = dfvec[dspec_peak]
-accel_est = (dfreq_est * wavelength) * 2
+    r = vel0*t + acel0*0.5*t**2
+    phase = np.mod(r / wavelength, 1) * np.pi * 2
+    signal = np.exp(1j * phase)
 
-print(f"Frequency drift estimation: {dfreq_est} Hz/s (real = {ddop0})")
-print(f"Acceleration estimation: {accel_est} m/s^2 (real = {acel0})")
+    boxes = signal_samps // cycle_size
+    tau0 = cycle_size * (boxes // 2)
 
-# correct signal
-signal = signal * np.exp(-1j*np.pi*2*dfreq_est*t**2)
-cspec = np.abs(fft.fft(signal))
+    stensil = np.full((signal_samps, ), True, dtype=bool)
+    for ind in range(boxes):
+        stensil[(cycle_size*ind):(cycle_size*ind + box_size)] = False
 
-df = 0.6e3
-ddf = 0.6e4
+    signal[stensil] = 0
 
-fig, axes = plt.subplots(4, 1)
+    fvec = fft.fftfreq(signal_samps, d=1.0/sample_rate)
+    spec = np.abs(fft.fft(signal))
 
-axes[0].plot(t, np.real(signal), label="Signal real component")
-axes[0].set_xlabel("Time [s]")
+    dsignal = signal[tau0:] * np.conj(signal[:-tau0])
+    # TODO: i dont remember why its 2*offset, can be seen in DAF-definition
+    step = 2*tau0/sample_rate
+    dsignal_samps = signal_samps - tau0
+
+    # Divide this with the step size to get to Hz/s
+    dfvec = fft.fftfreq(dsignal_samps, d=1.0/sample_rate) / step
+    dspec = np.abs(fft.fft(dsignal))
+    dspec_peak = np.argmax(dspec)
+    dfreq_est = dfvec[dspec_peak]
+    accel_est = (dfreq_est * wavelength) * 2
+
+    # correct signal
+    signal = signal * np.exp(-1j*np.pi*2*dfreq_est*t**2)
+    cspec = np.abs(fft.fft(signal))
+    vel_est = fvec[np.argmax(cspec)] * wavelength
+
+    if plot:
+        df = 0.6e3
+        ddf = 0.6e4
+        print(f"Frequency drift estimation: {dfreq_est} Hz/s (real = {ddop0})")
+        print(f"Acceleration estimation: {accel_est} m/s^2 (real = {acel0})")
+        fig, axes = plt.subplots(4, 1)
+
+        axes[0].plot(t, np.real(signal), label="Signal real component")
+        axes[0].set_xlabel("Time [s]")
+        axes[0].legend()
+
+        axes[1].plot(fvec, spec, ".-", label="Signal spectrum")
+        axes[1].set_xlim([dop0 - df, dop0 + df])
+        axes[1].axvline(dop0, c="r", ls="--", label="True doppler")
+        axes[1].set_xlabel("Frequency [Hz/s]")
+        axes[1].legend()
+
+        axes[2].plot(dfvec, dspec, ".-", label=f"Lagged spectrum @ tau={tau0}")
+        axes[2].axvline(ddop0, c="r", ls="--", label="True doppler drift")
+        axes[2].set_xlim([ddop0 - ddf, ddop0 + ddf])
+        axes[2].set_xlabel("Frequency drift [Hz/s]")
+        axes[2].legend()
+
+        axes[3].plot(fvec, cspec, ".-", label="Acceleration corrected signal spectrum")
+        axes[3].axvline(dop0, c="r", ls="--", label="True doppler")
+        axes[3].set_xlim([dop0 - df, dop0 + df])
+        axes[3].set_xlabel("Frequency [Hz/s]")
+        axes[3].legend()
+
+    return fvec, cspec, dfvec, dspec, vel_est, accel_est
+
+
+acel = -0.20e3
+analyse_cycle_daf(10, acel, plot=True)
+
+
+fig, axes = plt.subplots(2, 1)
+
+n_ipps = np.arange(4, 20)
+signal_times = n_ipps*cycle_size/sample_rate
+res_vecs = np.empty((len(n_ipps), 2))
+
+for ni, n_ipp in enumerate(n_ipps):
+    fvec, cspec, dfvec, dspec, vel_est, accel_est = analyse_cycle_daf(n_ipp, acel)
+    res_vecs[ni, 0] = vel_est
+    res_vecs[ni, 1] = accel_est
+
+    if n_ipp in [5, 10, 20]:
+        axes[0].plot((dfvec * wavelength) * 2, dspec, label=f"N_ipp={n_ipp}")
+        axes[1].plot(fvec * wavelength, cspec)
+
+axes[0].axvline(acel, ls="--", c="k")
 axes[0].legend()
+axes[0].set_xlim([-1e3, 1e3])
 
-axes[1].plot(fvec, spec, ".-", label="Signal spectrum")
-axes[1].set_xlim([dop0 - df, dop0 + df])
-axes[1].axvline(dop0, c="r", ls="--", label="True doppler")
-axes[1].set_xlabel("Frequency [Hz/s]")
-axes[1].legend()
 
-axes[2].plot(dfvec, dspec, ".-", label=f"Lagged spectrum @ tau={tau0}")
-axes[2].axvline(ddop0, c="r", ls="--", label="True doppler drift")
-axes[2].set_xlim([ddop0 - ddf, ddop0 + ddf])
-axes[2].set_xlabel("Frequency drift [Hz/s]")
-axes[2].legend()
+axes[1].axhline(vel0, ls="--", c="k")
+axes[1].set_xlim([-300 + vel0, 300 + vel0])
 
-axes[3].plot(fvec, cspec, ".-", label="Acceleration corrected signal spectrum")
-axes[3].axvline(dop0, c="r", ls="--", label="True doppler")
-axes[3].set_xlim([dop0 - df, dop0 + df])
-axes[3].set_xlabel("Frequency [Hz/s]")
-axes[3].legend()
+fig, axes = plt.subplots(2, 1)
+axes[0].plot(n_ipps, res_vecs[:, 0])
+axes[0].axhline(vel0, ls="--", c="r")
+axes[1].plot(n_ipps, res_vecs[:, 1])
+axes[1].axhline(acel, ls="--", c="r")
+
+fig, ax = plt.subplots()
+
+accs = np.linspace(0, 0.4e3, 100)
+err_vecs = np.empty((3, len(accs)))
+
+for ni, n_ipp in enumerate([5, 10, 20]):
+    for ai in range(len(accs)):
+        fvec, cspec, dfvec, dspec, vel_est, accel_est = analyse_cycle_daf(n_ipp, accs[ai])
+        err_vecs[ni, ai] = accel_est - accs[ai]
+
+    ax.plot(accs, err_vecs[ni, :], label=f"N_ipp={n_ipp}")
+ax.axhline(0, ls="--", c="r")
+ax.legend()
 
 plt.show()
--- examples/back_to_basics/part6_ipp_number.py
+++ examples/back_to_basics/part6_ipp_number.py
@@ -8,22 +8,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fft as fft
 import scipy.constants as constants
-
+from tqdm import tqdm
 sample_rate = 1000000
 
 
 ipp_samps = 20000
-baud_length = 40*16/sample_rate
+baud_length = 15*16/sample_rate
 code_len = 8
-tx_len = np.round(baud_length * sample_rate * code_len).astype(np.int64)
+T_tx_samps = np.round(baud_length * sample_rate * code_len).astype(np.int64)
 
-code = np.random.randint(0, high=2, size=(code_len,))
-code[code == 0] = -1
 wavelength = 0.3
 frequency = constants.c / wavelength
 
 
-def waveform_generator(t, baud_length, frequency, code):
+def waveform_generator(t, baud_length, code):
     t_ind = (t // baud_length).astype(np.int64)
     carrier = np.zeros(t.shape, dtype=np.complex128)
     inds = np.logical_and(t >= 0, t <= baud_length * len(code))
@@ -33,49 +31,76 @@ def waveform_generator(t, baud_length, frequency, code):
 
 # Determining acceleration
 range0 = 2000e3
-vel0 = 0.4e3
-dop0 = vel0 / wavelength
-acel0 = -0.20e3
+range_gate0 = np.round((range0 / constants.c) * sample_rate).astype(np.int64)
 
 
-def search_accel(decimation, n_ipp, accels, noise_sigma=None):
+def search_accel(decimation, n_ipp, accels, acel0, vel0, noise_sigma=None):
     dec_sample_rate = sample_rate / decimation
     peak_dop = np.empty_like(accels)
     peak = np.empty_like(accels)
 
+    z_rx = np.zeros((ipp_samps*n_ipp, ), dtype=np.complex64)
+    dec_z_rx = np.zeros((ipp_samps*n_ipp//decimation, ), dtype=np.complex64)
+
+    window_inds = np.concatenate([
+        np.arange(T_tx_samps) + ind*ipp_samps
+        for ind in range(n_ipp)
+    ])
+    dec_window_inds = np.concatenate([
+        np.arange(T_tx_samps//decimation) + ind*ipp_samps//decimation
+        for ind in range(n_ipp)
+    ])
+
+    t_tx = np.arange(T_tx_samps) / sample_rate
+
+    for ind in range(n_ipp):
+        signal = np.zeros((ipp_samps,), dtype=np.complex64)
+        samp0 = ind * ipp_samps
+        t0 = samp0 / sample_rate
+
+        code = np.random.randint(0, high=2, size=(code_len,))
+        code[code == 0] = -1
+        tx_wave = waveform_generator(t_tx, baud_length, code)
+
+        signal[0:T_tx_samps] += tx_wave
+
+        r0 = range0 + vel0*t0 + 0.5*acel0*t0**2
+        v0 = vel0 + acel0*t0
+        a0 = acel0
+
+        rg0 = np.round((r0 / constants.c) * sample_rate).astype(np.int64)
+        rg_samp0 = rg0
+
+        ranges = r0 + v0*t_tx + 0.5*a0*t_tx**2
+        phase = np.mod(ranges / wavelength, 1) * np.pi * 2
+
+        rx_wave = tx_wave * np.exp(1j * phase)
+
+        if noise_sigma is not None:
+            rx_wave += noise_sigma * (np.random.randn(*rx_wave.shape) + 1j * np.random.randn(*rx_wave.shape))
+
+        signal[rg_samp0:(rg_samp0 + T_tx_samps)] += rx_wave
+        z_rx[(ind * ipp_samps):((ind + 1) * ipp_samps)] = signal
+
     for ai, acc in enumerate(accels):
+        ztx = z_rx[window_inds]
+        zrx = z_rx[window_inds + range_gate0]
 
-        dec_z_rx = np.zeros((ipp_samps*n_ipp//decimation, ), dtype=np.complex128)
-        dec_ajs = np.zeros((ipp_samps*n_ipp//decimation, ), dtype=np.complex128)
+        tx_pwr = np.sum(np.abs(ztx) ** 2.0)
+        tx_amp = np.sqrt(tx_pwr)
+        ztx = np.conj(ztx) / tx_amp
 
-        for ind in range(n_ipp):
-            samps0 = np.arange(tx_len)
-            samps = samps0 + ind*ipp_samps
-            dec_samps = np.arange(tx_len//decimation) + ind*ipp_samps//decimation
-            t0 = samps0 / sample_rate
-            t = samps / sample_rate
+        t = window_inds/sample_rate
+        dec_t = t[::decimation]
+        dec_t2 = dec_t**2
 
-            dec_t = t[::decimation]
-            dec_t2 = dec_t**2
+        echo = zrx * ztx
+        dec_echo = np.sum(echo.reshape(-1, decimation), axis=-1)
 
-            r = range0 + vel0*t + acel0*0.5*t**2
-            phase = np.mod(r / wavelength, 1) * np.pi * 2
-            tx_code = waveform_generator(t0, baud_length, frequency, code)
-            signal = np.exp(1j * phase) * tx_code
+        dec_aj = np.exp(-1j * np.pi / wavelength * acc * dec_t2)
+        dec_echo *= dec_aj
 
-            dec_aj = np.exp(-1j * np.pi / wavelength * acc * dec_t2)
-            dec_ajs[dec_samps] = dec_aj
-
-            if noise_sigma is not None:
-                signal += noise_sigma * (np.random.randn(*signal.shape) + 1j * np.random.randn(*signal.shape))
-
-            echo = signal * np.conj(tx_code)
-
-            dec_echo = np.sum(echo.reshape(-1, decimation), axis=-1)
-            dec_z = dec_echo.copy()
-            dec_z *= dec_aj
-
-            dec_z_rx[dec_samps] = dec_z
+        dec_z_rx[dec_window_inds] = dec_echo
 
         fvec = fft.fftfreq(len(dec_z_rx), d=1.0/dec_sample_rate)
         spec = np.abs(fft.fft(dec_z_rx))
@@ -102,23 +127,70 @@ axes[1].semilogy(signal_ipps, 0.5*accel_res, label="Acceleration resolution")
 for ax in axes:
     ax.set_xticks(signal_ipps)
     ax.grid(True)
-plt.show()
 
 accels = np.linspace(-0.3e3, 0.3e3, 300)
+acel = -0.20e3
+vel = 0.4e3
+dop0 = vel / wavelength
 
-for ns in [None, 1.0e1]:
+for ns in [None, 3.0]:
 
     fig, axes = plt.subplots(2, 1)
 
     for n_ipp in [1, 2, 5, 10]:
-        peak_dop, peak = search_accel(5, n_ipp, accels, noise_sigma=ns)
+        peak_dop, peak = search_accel(16, n_ipp, accels, acel, vel, noise_sigma=ns)
 
         axes[0].plot(accels, peak_dop, label=f"N_ipp={n_ipp}")
         axes[1].plot(accels, peak)
 
     axes[0].axhline(dop0, ls="--", c="k")
-    axes[1].axvline(acel0, ls="--", c="k")
+    axes[1].axvline(acel, ls="--", c="k")
     axes[0].legend()
 
+cs = ["b", "c", "g"]
+fig, ax = plt.subplots()
+
+accs = np.linspace(0, 0.2e3, 20)
+err_vecs_acc = np.empty((3, len(accs)))
+pbar = tqdm(total=err_vecs_acc.size)
+for ni, n_ipp in enumerate([5, 10, 20]):
+    for ai in range(len(accs)):
+        peak_dop, peak = search_accel(16, n_ipp, accels, accs[ai], vel, noise_sigma=None)
+        err_vecs_acc[ni, ai] = accels[np.argmax(peak)] - accs[ai]
+        pbar.update(1)
+    signal_times = n_ipp * ipp_samps / sample_rate
+    freq_drift_lim = 1.0 / signal_times**2
+    ax.axhline(-freq_drift_lim*0.5, ls="--", c=cs[ni])
+    ax.axhline(freq_drift_lim*0.5, ls="--", c=cs[ni])
+    ax.plot(accs, err_vecs_acc[ni, :], c=cs[ni], label=f"N_ipp={n_ipp}")
+pbar.close()
+
+ax.axhline(0, ls="--", c="r")
+ax.set_xlabel("True acceleration [m/s^2]")
+ax.set_ylabel("Found acceleration error [m/s^2]")
+ax.legend()
+
+
+fig, ax = plt.subplots()
+
+vels = np.linspace(0, 1e3, 20)
+err_vecs_vel = np.empty((3, len(vels)))
+pbar = tqdm(total=err_vecs_vel.size)
+for ni, n_ipp in enumerate([5, 10, 20]):
+    for vi in range(len(vels)):
+        peak_dop, peak = search_accel(16, n_ipp, accels, acel, vels[vi], noise_sigma=None)
+        err_vecs_vel[ni, vi] = accels[np.argmax(peak)] - acel
+        pbar.update(1)
+    signal_times = n_ipp * ipp_samps / sample_rate
+    freq_drift_lim = 1.0 / signal_times**2
+    ax.axhline(-freq_drift_lim*0.5, ls="--", c=cs[ni])
+    ax.axhline(freq_drift_lim*0.5, ls="--", c=cs[ni])
+    ax.plot(vels, err_vecs_vel[ni, :], c=cs[ni], label=f"N_ipp={n_ipp}")
+pbar.close()
+
+ax.axhline(0, ls="--", c="r")
+ax.set_xlabel("True velocity [m/s^2]")
+ax.set_ylabel("Found acceleration error [m/s^2]")
+ax.legend()
 
 plt.show()
