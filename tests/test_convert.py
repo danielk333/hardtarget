ca9711abf06b94ec8d2559e310d55550ef4c2638
Ingar Arntzen <inar@norceresearch.no>
2024-09-09 09:48:51 +0200

    removed obsolete gmf test in test_convert

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -13,9 +13,6 @@ import bisect
 test products converted to Hardtarget DRF in various ways
 """
 
-
-
-
 PROJECT = Path("/cluster/projects/p106119-SpaceDebrisRadarCharacterization")
 RAW = PROJECT / "raw" 
 DRF = PROJECT / "drf"
@@ -89,78 +86,6 @@ def test_drf_equivalence():
     npt.assert_equal(old_data, new_data)
 
 
-
-GMF = PROJECT / "gmf"
-NEWGMF = PROJECT / "test/debug-2024-09-06/ingar_gmf"
-
-
-
-def test_gmf_equivalence():
-    """
-    testing that gmf data is time-aligned the same,
-    whether it is based on old DRF or new DRF 
-
-    GMF is analyzed using DRF
-
-    NEW GMF is analyzed from NEW DRF, for 1 minute
-    -s 2021-11-23T10:00:00 -e 2021-11-23T10:01:00
-    """
-
-    old_gmf = GMF / PRODUCT
-    TS = ts_from_str("2021-11-23T10:00:00.0")
-
-
-    def get_data_from_product(product, ts):
-
-        # need to find the correct gmf files within gmf products
-        # gmf filenames are in unix time microseconds of first sample in file
-        # gmf also has metadata which I can use
-        files = sorted(utils.all_gmf_h5_files(str(product)))
-
-        # choose one specific point in time
-        # find the file which covers this ts
-
-        def get_ts_from_path(p):
-            return float (p.name.split('-')[1].split('.')[0]) / 1e6
-
-        def is_relevant(f):
-            file_start_ts = get_ts_from_path(f)
-            file_end_ts = file_start_ts + 2.0
-            return file_start_ts <= ts and ts < file_end_ts
-
-        relevant_files = [f for f in files if is_relevant(f)]
-        print(relevant_files)
-        relevant_file = relevant_files[0]
-
-        file_start_ts = get_ts_from_path(relevant_file)
-        delta = ts - file_start_ts
-        print("delta", delta)
-
-
-
-        with h5py.File(relevant_file, "r") as f:        
-            #chnl = f["experiment"].attrs["rx_channel"]
-            #ipp = f["experiment"].attrs["ipp"]
-            #n_ipp = f["processing"].attrs["n_ipp"]
-            #num_cohints_per_file = f["processing"].attrs["num_cohints_per_file"]
-            sample_rate = f["experiment"].attrs["sample_rate"]
-            epoch = f['epoch_unix'][()]
-            print(epoch)
-
-
-            print(list(f.keys()))
-            print(f['sample_numbers'][:])
-
-            idx = index_from_ts(ts, sample_rate, ts_offset_sec=epoch)
-            print("idx", idx)
-
-
-
-        # find index correspoinding to TS
-        
-
-
-    get_data_from_product(old_gmf, TS)
 
 
 
